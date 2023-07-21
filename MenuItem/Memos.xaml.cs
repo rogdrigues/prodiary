1656491b7eb318259fa0da412f91d4727cac0bb6
phong <phongnhhe163436@fpt.edu.vn>
2023-07-21 13:51:35 +0700

    Update Diary Memo

--- MenuItem/Memos.xaml.cs
+++ MenuItem/Memos.xaml.cs
@@ -1,6 +1,8 @@
-﻿using ProDiaryApplication.Models;
+﻿using Microsoft.Win32;
+using ProDiaryApplication.Models;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -21,6 +23,8 @@ namespace ProDiaryApplication.MenuItem
     /// </summary>
     public partial class Memos : Window
     {
+        private RichTextBox focusedRichTextBox = null;
+
         public Memos()
         {
             InitializeComponent();
@@ -74,6 +78,200 @@ namespace ProDiaryApplication.MenuItem
             }
 
             return image;
+        }
+
+        private void btnWordAction(object sender, RoutedEventArgs e)
+        {
+            Button btn = sender as Button;
+
+            if(btn.Name == "btnUndo")
+            {
+                FormatText("Undo");
+            }
+            else if(btn.Name == "btnRedo")
+            {
+                FormatText("Redo");
+            }
+            else if (btn.Name == "btnImportImage")
+            {
+                FormatText("Image");
+            }
+            else if (btn.Name == "btnAlignLeft")
+            {
+                FormatText("AlignLeft");
+            }
+            else if (btn.Name == "btnAlignJustify")
+            {
+                FormatText("AlignJustify");
+            }
+            else if (btn.Name == "btnAlignRight")
+            {
+                FormatText("AlignRight");
+            }
+            else if (btn.Name == "btnAlignFormat")
+            {
+                FormatText("AlignCenter");
+            }
+            else if (btn.Name == "btnSizeIncrease")
+            {
+                FormatText("SizeIncrease");
+            }
+            else if (btn.Name == "btnSizeDecrease")
+            {
+                FormatText("SizeDecrease");
+            }
+            else if (btn.Name == "btnBold")
+            {
+                FormatText("Bold");
+            }
+            else if (btn.Name == "btnItalic")
+            {
+                FormatText("Italic");
+            }
+            else if (btn.Name == "btnUnderline")
+            {
+                FormatText("Underline");
+            }
+            else if (btn.Name == "btnCut")
+            {
+                FormatText("Cut");
+            }
+            else if (btn.Name == "btnCopy")
+            {
+                FormatText("Copy");
+            }
+            else if (btn.Name == "btnPaste")
+            {
+                FormatText("Paste");
+            }else if(btn.Name == "btnImportImage")
+            {
+                FormatText("Image");
+            }
+        }
+        private void FormatText(string action)
+        {
+            RichTextBox richTextBox = focusedRichTextBox;
+            TextSelection selectedText = richTextBox?.Selection;
+
+            if (richTextBox != null && selectedText != null)
+            {
+                switch (action)
+                {
+                    case "Undo":
+                        if (richTextBox.CanUndo)
+                            richTextBox.Undo();
+                        break;
+                    case "Redo":
+                        if (richTextBox.CanRedo)
+                            richTextBox.Redo();
+                        break;
+                    case "Cut":
+                        if (!selectedText.IsEmpty)
+                            richTextBox.Cut();
+                        break;
+                    case "Copy":
+                        if (!selectedText.IsEmpty)
+                            richTextBox.Copy();
+                        break;
+                    case "Paste":
+                        if (Clipboard.ContainsText())
+                        {
+                            int caretPosition = richTextBox.CaretPosition.GetOffsetToPosition(richTextBox.Document.ContentStart);
+                            richTextBox.CaretPosition.InsertTextInRun(Clipboard.GetText());
+
+                            int newPositionOffset = caretPosition + Clipboard.GetText().Length;
+                            TextPointer newPosition = richTextBox.Document.ContentStart.GetPositionAtOffset(newPositionOffset, LogicalDirection.Forward);
+                            if (newPosition != null)
+                            {
+                                richTextBox.CaretPosition = newPosition;
+                            }
+                        }
+                        break;
+                    case "AlignLeft":
+                        richTextBox.Selection.ApplyPropertyValue(Paragraph.TextAlignmentProperty, TextAlignment.Left);
+                        break;
+                    case "AlignJustify":
+                        richTextBox.Selection.ApplyPropertyValue(Paragraph.TextAlignmentProperty, TextAlignment.Justify);
+                        break;
+                    case "AlignRight":
+                        richTextBox.Selection.ApplyPropertyValue(Paragraph.TextAlignmentProperty, TextAlignment.Right);
+                        break;
+                    case "AlignCenter":
+                        richTextBox.Selection.ApplyPropertyValue(Paragraph.TextAlignmentProperty, TextAlignment.Center);
+                        break;
+                    case "SizeIncrease":
+                        richTextBox.Selection.ApplyPropertyValue(TextElement.FontSizeProperty, richTextBox.Selection.GetPropertyValue(TextElement.FontSizeProperty));
+                        break;
+                    case "SizeDecrease":
+                        richTextBox.Selection.ApplyPropertyValue(TextElement.FontSizeProperty, richTextBox.Selection.GetPropertyValue(TextElement.FontSizeProperty));
+                        break;
+                    case "Bold":
+                        if (!selectedText.IsEmpty)
+                        {
+                            TextRange tr = new TextRange(selectedText.Start, selectedText.End);
+                            object fontWeight = tr.GetPropertyValue(TextElement.FontWeightProperty);
+                            FontWeight boldWeight = fontWeight is FontWeight weight && weight == FontWeights.Bold ? FontWeights.Normal : FontWeights.Bold;
+                            tr.ApplyPropertyValue(TextElement.FontWeightProperty, boldWeight);
+                        }
+                        break;
+                    case "Italic":
+                        if (!selectedText.IsEmpty)
+                        {
+                            TextRange tr = new TextRange(selectedText.Start, selectedText.End);
+                            object fontStyle = tr.GetPropertyValue(TextElement.FontStyleProperty);
+                            FontStyle italicStyle = fontStyle is FontStyle style && style == FontStyles.Italic ? FontStyles.Normal : FontStyles.Italic;
+                            tr.ApplyPropertyValue(TextElement.FontStyleProperty, italicStyle);
+                        }
+                        break;
+                    case "Underline":
+                        if (!selectedText.IsEmpty)
+                        {
+                            TextRange tr = new TextRange(selectedText.Start, selectedText.End);
+                            object textDecorations = tr.GetPropertyValue(Inline.TextDecorationsProperty);
+                            TextDecorationCollection underlineCollection = textDecorations is TextDecorationCollection decorations && decorations.Count > 0 ? null : TextDecorations.Underline;
+                            tr.ApplyPropertyValue(Inline.TextDecorationsProperty, underlineCollection);
+                        }
+                        break;
+                    case "Image":
+                        InsertImage_Click();
+                        break;
+                }
+            }
+        }
+        private void InsertImage_Click()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files (*.jpg;*.jpeg;*.png;*.bmp)|*.jpg;*.jpeg;*.png;*.bmp|All files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string imagePath = openFileDialog.FileName;
+                CreateImageControl(imagePath);
+            }
+        }
+        private void CreateImageControl(string imagePath)
+        {
+            Image image = new Image();
+            BitmapImage bitmapImage = new BitmapImage(new Uri(imagePath));
+            image.Source = bitmapImage;
+
+
+            // Đặt kích thước cho hình ảnh (tùy chọn)
+            image.Width = 200;
+            image.Height = 150;
+
+            // Thêm hình ảnh vào StackPanel diaryBox
+            diaryBox.Children.Add(image);
+        }
+        [DebuggerHidden]
+        private void rtbDescription_PreviewGotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            focusedRichTextBox = sender as RichTextBox;
+        }
+
+        [DebuggerHidden]
+        private void rtbDescription_PreviewLostKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            //focusedRichTextBox = null;
         }
     }
 }
