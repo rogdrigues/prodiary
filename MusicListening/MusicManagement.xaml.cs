0951cca81f826fa0ca8b1f1678062d584fbad4c9
phtphtxxzz <quansuhongtruong@gmail.com>
2023-07-23 16:45:31 +0700

    design

---
+++ MusicListening/MusicManagement.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProDiaryApplication.MusicListening
+{
+    /// <summary>
+    /// Interaction logic for MusicManagement.xaml
+    /// </summary>
+    public partial class MusicManagement : Page
+    {
+        public MusicManagement()
+        {
+            InitializeComponent();
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.ChangedButton == MouseButton.Left)
+            {
+            }
+        }
+
+        private void SongItem_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ UserControls/PopularSong.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProDiaryApplication.UserControls
+{
+    /// <summary>
+    /// Interaction logic for PopularSong.xaml
+    /// </summary>
+    public partial class PopularSong : Page
+    {
+        public PopularSong()
+        {
+            InitializeComponent();
+        }
+        public PopularSong(string title, string author)
+        {
+            Title = title;
+            Author = author;
+        }
+
+        public string Title { get; set; }
+        public string Category { get; set; } = null!;
+        public string Author { get; set; } = null!;
+        public string Time { get; set; }
+    }
+}
